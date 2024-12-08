7cb78400db740f12975b3258a051836e6e2273fc
Thomson simbolon <simbolonthomson10@gmail.com>
2024-12-08 20:41:22 +0700

    Update App E-Course

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./dist/css/style.css";
-
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
